fcc490dff5ab2be2b9ffef1718ee737fc679fe0d
Sara De Clara <saradeclara@gmail.com>
2024-07-10 11:49:27 +0100

    unused import removed

--- src/components/ProjectGrid/ProjectGrid.tsx
+++ src/components/ProjectGrid/ProjectGrid.tsx
@@ -6,45 +6,44 @@ import SingleProject from "./SingleProject";
 import { GithubProject } from "@/types/ProjectGrid";
 import { allowedRepos } from "@/src/app/data";
 import { centeredFlex } from "@/src/styles/globalClasses";
-import SingleLoading from "./SingleLoading";
 
 const ProjectGrid = () => {
-  const { data, isLoading, error } = useFetchData<GithubProject[]>(
-    "https://api.github.com/users/saradeclara/repos",
-    process.env.GITHUB_TOKEN,
-  );
+	const { data, isLoading, error } = useFetchData<GithubProject[]>(
+		"https://api.github.com/users/saradeclara/repos",
+		process.env.GITHUB_TOKEN
+	);
 
-  if (isLoading)
-    return (
-      <Box>
-        <LoadingGrid />
-      </Box>
-    );
-  if (error) return <Box>ERROR!</Box>;
+	if (isLoading)
+		return (
+			<Box>
+				<LoadingGrid />
+			</Box>
+		);
+	if (error) return <Box>ERROR!</Box>;
 
-  const allowedProjects = data
-    ? [...data].filter((singleRepo) =>
-        allowedRepos
-          .map((allowedRepo) => allowedRepo.name)
-          .includes(singleRepo.name),
-      )
-    : null;
+	const allowedProjects = data
+		? [...data].filter((singleRepo) =>
+				allowedRepos
+					.map((allowedRepo) => allowedRepo.name)
+					.includes(singleRepo.name)
+			)
+		: null;
 
-  return (
-    <Box sx={{ ...centeredFlex }}>
-      {allowedProjects
-        ? allowedProjects.map((singleProject, index) => {
-            return (
-              <SingleProject
-                key={index}
-                allowedRepos={allowedRepos}
-                singleProject={singleProject}
-              />
-            );
-          })
-        : null}
-    </Box>
-  );
+	return (
+		<Box sx={{ ...centeredFlex }}>
+			{allowedProjects
+				? allowedProjects.map((singleProject, index) => {
+						return (
+							<SingleProject
+								key={index}
+								allowedRepos={allowedRepos}
+								singleProject={singleProject}
+							/>
+						);
+					})
+				: null}
+		</Box>
+	);
 };
 
 export default ProjectGrid;
